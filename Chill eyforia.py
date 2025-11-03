8ba4a5eb33441081112aa62bebbb03f31edad7b0
Dog Jockey (DJ) <tuprofedog@gmail.com>
2025-11-02 18:16:43 -0600

    rotate colors

--- Chill eyforia.py
+++ Chill eyforia.py
@@ -1,19 +1,10 @@
 #Pixel Art - http://www.101computing.net/pixel-art-in-python/
 import turtle
 from config import *
+from time import sleep
 
 myPen = turtle.Turtle()
-myPen._tracer(0)
-myPen.speed(0)
 boxSize = 25
-
-# Position myPen in top left area of the screen
-myPen.penup()
-myPen.goto(0, 0)
-myPen.forward(-400) # Horizontal position adjusted
-myPen.setheading(90)
-myPen.forward(250) # Vertical position adjusted
-myPen.setheading(0)
 
 # This function draws a box by drawing each side of the square and using the fill function
 def box(intDim):
@@ -52,7 +43,37 @@ def drawPixelArt(pixelArt, colors):
     
     myPen.getscreen().update()	
 	
-drawPixelArt(FACE_1, COLORS_1)
+# Returns the rotated list
+def rightRotate(lists, num):
+    output_list = []
 
-from time import sleep
-sleep(5)
+    # Will add values from n to the new list
+    for item in range(len(lists) - num, len(lists)):
+        output_list.append(lists[item])
+
+    # Will add the values before
+    # n to the end of new list
+    for item in range(0, len(lists) - num):
+        output_list.append(lists[item])
+
+    return output_list
+
+for i in range(1, len(COLORS_1) + 1):
+    for j in range(0, len(COLORS_1)):
+        turtle.clearscreen()
+
+        # Initial config
+        myPen._tracer(0)
+
+        # Position myPen in top left area of the screen
+        myPen.penup()
+        myPen.goto(0, 0)
+        myPen.forward(-400) # Horizontal position adjusted
+        myPen.setheading(90)
+        myPen.forward(250) # Vertical position adjusted
+        myPen.setheading(0)
+
+        # Draw the pixel art and rotate colors
+        drawPixelArt(FACE_1, COLORS_1)
+        COLORS_1 = rightRotate(COLORS_1, i)
+        sleep(1)
